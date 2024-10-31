ae7b8059663704ac8757153b6447361a9647e914
heygwangjin <heygwangjin@gmail.com>
2024-10-31 21:36:51 +0900

    style: Add style in login

--- src/routes/demo/lucia/login/+page.svelte
+++ src/routes/demo/lucia/login/+page.svelte
@@ -6,14 +6,19 @@
 </script>
 
 <h1>Login/Register</h1>
-<form method="post" action="?/login" use:enhance>
-	<label>
-		Username
-		<input name="username" />
+<form
+	method="post"
+	action="?/login"
+	use:enhance
+	class="mt-2 flex flex-col gap-4 rounded border-2 border-blue-400 p-2"
+>
+	<label class="flex items-center gap-2">
+		Username :
+		<input name="username" class="rounded border-2 border-gray-300" />
 	</label>
 	<label>
-		Password
-		<input type="password" name="password" />
+		Password :
+		<input type="password" name="password" class="rounded border-2 border-gray-300" />
 	</label>
 	<button>Login</button>
 	<button formaction="?/register">Register</button>
