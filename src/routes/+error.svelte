ccf22152fd2530f83005f839be805c008f10dfae
heygwangjin <heygwangjin@gmail.com>
2024-11-02 17:31:49 +0900

    feat: Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { browser } from '$app/environment';
+
+	// we don't want to use <svelte:window bind:online> here,
+	// because we only care about the online state when
+	// the page first loads
+	const online = browser ? navigator.onLine : true;
+</script>
+
+<svelte:head>
+	<title>{$page.status}</title>
+</svelte:head>
+
+<div class="outer">
+	<div class="inner">
+		{#if online}
+			{#if $page.status === 404}
+				<h1>Not Found!</h1>
+				<p>요청하신 페이지를 찾을 수 없습니다.</p>
+				<p>
+					만약 해당 페이지가 존재해야 한다면, <a href="/qna"> 문의사항</a>을 남겨주세요!
+				</p>
+			{:else}
+				<h1>Yikes!</h1>
+
+				<p>서버 오류가 발생했습니다. 페이지를 새로고침해주세요.</p>
+				<p>
+					에러가 계속 발생한다면, <a href="/qna">문의사항</a>을 남겨주세요!
+				</p>
+			{/if}
+		{:else}
+			<h1>OFFLINE!</h1>
+			<p>인터넷 연결이 되면 페이지를 새로고침해주세요.</p>
+		{/if}
+	</div>
+</div>
+
+<style>
+	.outer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 0 var(--hh-page-padding-side);
+		width: 100%;
+		height: 100%;
+	}
+
+	.inner {
+		max-width: 48rem;
+		text-align: center;
+		text-wrap: balance;
+
+		a {
+			text-wrap: nowrap;
+			color: var(--hh-destructive);
+		}
+	}
+
+	p {
+		margin: 1em auto;
+		font: var(--hh-font-body-large);
+	}
+
+	h1 {
+		font: var(--hh-font-h1);
+	}
+</style>
