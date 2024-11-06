4ba04c36538eb51b2084be1fdf7629f7d94f7859
heygwangjin <heygwangjin@gmail.com>
2024-11-06 22:48:55 +0900

    chore: seo
    * fix: meta tag for sed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,13 @@
 	let { data, children: layout_children } = $props();
 </script>
 
+<svelte:head>
+	<meta
+		name="Keywords"
+		content="미용사, 미용실, 구인, 구직, 취업, 채용, 구직사이트, 구인사이트, 미용사구인, 미용사구직, 미용실구인, 미용실구직, 헤어인잡, 미용커플"
+	/>
+</svelte:head>
+
 <Shell>
 	{#snippet top_nav()}
 		<Nav title={data.nav_title} links={data.nav_links} />
