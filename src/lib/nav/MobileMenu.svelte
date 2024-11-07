d6d8ebbdf232e88765ad0791a0379fc285695eef
heygwangjin <heygwangjin@gmail.com>
2024-11-08 04:01:23 +0900

    style: Display active mobile menu

--- src/lib/nav/MobileMenu.svelte
+++ src/lib/nav/MobileMenu.svelte
@@ -1,31 +1,44 @@
 <script lang="ts">
 	import Icon from '$lib/components/Icon.svelte';
+	import { page } from '$app/stores';
 </script>
 
 <nav>
 	<div>
-		<a href="/" aria-label="홈 (메인)">
+		<a href="/" class={$page.url.pathname === '/' ? 'active' : ''} aria-label="홈 (메인)">
 			<Icon name="home" />
 		</a>
 		<span>홈</span>
 	</div>
 
 	<div>
-		<a href="/jobs" aria-label="채용정보">
+		<a
+			href="/jobs"
+			class={$page.url.pathname.startsWith('/jobs') ? 'active' : ''}
+			aria-label="채용정보"
+		>
 			<Icon name="file-text" />
 		</a>
 		<span>채용정보</span>
 	</div>
 
 	<div>
-		<a href="/seekers" aria-label="인재정보">
+		<a
+			href="/seekers"
+			class={$page.url.pathname.startsWith('/seekers') ? 'active' : ''}
+			aria-label="인재정보"
+		>
 			<Icon name="users" />
 		</a>
 		<span>인재정보</span>
 	</div>
 
 	<div>
-		<a href="/recently-viewed-jobs" aria-label="최근 본 채용정보">
+		<a
+			href="/recently-viewed-jobs"
+			class={$page.url.pathname.startsWith('/recently-viewed-jobs') ? 'active' : ''}
+			aria-label="최근 본 채용정보"
+		>
 			<Icon name="list" />
 		</a>
 		<span>최근 본</span>
@@ -76,6 +89,10 @@
 
 	span {
 		color: var(--hp-fg-3);
+	}
+
+	.active {
+		color: var(--hp-fg-1);
 	}
 
 	@media screen and (min-width: 1024px) {
