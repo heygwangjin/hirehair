5af0983c34d9dc93d7b9d00b21115580696c4f0d
heygwangjin <heygwangjin@gmail.com>
2025-01-04 06:51:48 +0900

    chore: Update description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,6 @@
 <svelte:head>
-	<title>헤어피플 | 미용사 구인 구직 사이트</title>
-
-	<meta
-		name="Description"
-		content="헤어피플은 미용실에서 일하고 싶은 미용사들과 미용사를 구인하는 미용실들을 연결해드립니다."
-	/>
+	<title>헤어피플 | 미용전문 구인구직 플랫폼</title>
+	<meta name="Description" content="미용 구인구직은 헤어피플에서" />
 </svelte:head>
 
 <div>
