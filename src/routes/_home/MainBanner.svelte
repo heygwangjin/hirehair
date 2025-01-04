6c7f9b0b89bd5db246b8ec03f227f72396f18b64
heygwangjin <heygwangjin@gmail.com>
2025-01-04 21:06:30 +0900

    refactor: Refactor main banner

---
+++ src/routes/_home/MainBanner.svelte
@@ -0,0 +1,34 @@
+<div class="main-banner-container">
+	<div class="main-banner-subcontainer">
+		<h1>가까운 곳에서 <br />시작하는 새로운 기회</h1>
+		<h2>내 주변 일자리를 한눈에 확인해보세요.</h2>
+	</div>
+</div>
+
+<style>
+	h1 {
+		font: var(--hp-font-banner-h1);
+	}
+
+	h2 {
+		font: var(--hp-font-banner-h2);
+	}
+
+	.main-banner-container {
+		position: relative;
+		height: var(--hp-banner-height);
+		width: 100vw;
+		left: 50%;
+		right: 50%;
+		margin-left: -50vw;
+		margin-right: -50vw;
+		background: var(--hp-bg-3);
+	}
+
+	.main-banner-subcontainer {
+		margin: 0 auto;
+		height: 100%;
+		max-width: var(--hp-page-max-width);
+		padding: var(--hp-banner-padding-block) var(--hp-banner-padding-side);
+	}
+</style>
